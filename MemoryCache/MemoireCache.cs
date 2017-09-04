56705a8e0ac7df211d50f7f14fd848edc3fffef2
clayton.kuhn <claytonedk@gmail.com>
2017-09-04 11:15:14 -0400

    Using MemoryCache in c# projects

---
+++ MemoryCache/MemoireCache.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Caching;
+
+namespace MemoryCacheExample
+{
+    public class MemoireCache<T> where T : class
+    {
+        private MemoryCache _cache;
+
+        public MemoireCache()
+        {
+            _cache = MemoryCache.Default;
+        }
+
+        public void AjouterObjet(string cle, T obj)
+        {
+            _cache.Add(cle, obj, DateTime.Now.AddMinutes(2));
+        }
+
+        public void AjouterObjet(string cle, IEnumerable<T> obj)
+        {
+            _cache.Add(cle, obj, DateTime.Now.AddMinutes(2));
+        }
+
+        public T ObtenirOuAjouterObjet(string cle, Func<T> func)
+        {
+            return _cache.LazyAddOrGetExitingItem<T>(cle, func, DateTime.Now.AddMinutes(2));
+        }
+
+        public IEnumerable<T> ObtenirOuAjouterListeObjet(string cle, Func<IEnumerable<T>> func)
+        {
+            return _cache.LazyAddOrGetExitingItem<IEnumerable<T>>(cle, func, DateTime.Now.AddMinutes(2));
+        }
+
+        public T GetObjet(string cle)
+        {
+            return _cache.Get(cle) as T;
+        }
+
+        public IEnumerable<T> GetListe(string cle)
+        {
+            return _cache.Get(cle) as IEnumerable<T>;
+        }
+    }
+}
---
+++ MemoryCache/MemoryCacheExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Runtime.Caching;
+
+namespace MemoryCacheExample
+{
+    public static class MemoryCacheExtensions
+    {
+        public static T LazyAddOrGetExitingItem<T>(this MemoryCache memoryCache, string key, Func<T> getItemFunc, DateTimeOffset absoluteExpiration)
+        {
+            var item = new Lazy<T>(
+                () => getItemFunc(),
+                LazyThreadSafetyMode.PublicationOnly // Do not cache lazy exceptions
+            );
+
+            var cachedValue = memoryCache.AddOrGetExisting(key, item, absoluteExpiration) as Lazy<T>;
+
+            return (cachedValue != null) ? cachedValue.Value : item.Value;
+        }
+    }
+}
